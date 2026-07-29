51656666f63861d4d0f4d26eeb3af77db155e429
darrellnth2023-ux <darrellnth2023@gmail.com>
2026-07-29 05:30:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,65 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-let gameState = { players: {}, pool: [], table: [] };
+// Create 106-tile Rummikub deck
+function createDeck() {
+  const colors = ['red', 'blue', 'yellow', 'black'];
+  let deck = [];
+  let id = 1;
+  colors.forEach(color => {
+    for (let num = 1; num <= 13; num++) {
+      deck.push({ id: id++, num, color });
+      deck.push({ id: id++, num, color });
+    }
+  });
+  deck.push({ id: id++, num: 'J', color: 'wild', joker: true });
+  deck.push({ id: id++, num: 'J', color: 'wild', joker: true });
+  return deck.sort(() => Math.random() - 0.5);
+}
+
+let pool = createDeck();
+let players = {};
+let tableSets = [];
 
 io.on('connection', (socket) => {
-  socket.on('joinGame', (playerName) => {
-    gameState.players[socket.id] = { name: playerName, rack: [] };
-    io.emit('stateUpdate', gameState);
+  socket.on('joinGame', (name) => {
+    // Deal 14 tiles on join
+    if (!players[socket.id]) {
+      let hand = pool.splice(0, 14);
+      players[socket.id] = { id: socket.id, name: name || 'Player', rack: hand };
+    }
+    io.emit('gameUpdate', { players, poolCount: pool.length, tableSets });
+  });
+
+  socket.on('drawTile', () => {
+    if (pool.length > 0 && players[socket.id]) {
+      const tile = pool.pop();
+      players[socket.id].rack.push(tile);
+      io.emit('gameUpdate', { players, poolCount: pool.length, tableSets });
+    }
+  });
+
+  socket.on('playTileToBoard', (tileId) => {
+    const player = players[socket.id];
+    if (player) {
+      const tileIdx = player.rack.findIndex(t => t.id === tileId);
+      if (tileIdx !== -1) {
+        const [tile] = player.rack.splice(tileIdx, 1);
+        tableSets.push([tile]);
+        io.emit('gameUpdate', { players, poolCount: pool.length, tableSets });
+      }
+    }
   });
 
   socket.on('disconnect', () => {
-    delete gameState.players[socket.id];
-    io.emit('stateUpdate', gameState);
+    delete players[socket.id];
+    if (Object.keys(players).length === 0) {
+      pool = createDeck();
+      tableSets = [];
+    }
+    io.emit('gameUpdate', { players, poolCount: pool.length, tableSets });
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server live on port ${PORT}`));
